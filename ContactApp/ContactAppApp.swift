3aea5454eb19ca880d657d7a1d6f3297dc56b3e3
Kaustubh Gadakh <kaustubhgadakh2001@gmail.com>
2024-08-07 20:18:26 +0530

    Initial Commit

---
+++ ContactApp/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by Kaustubh Kishor Gadakh on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
